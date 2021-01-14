2e3fc442afe9f8a2ebc65f04fc08396de759d94e
Sherzod Nimatullo <shermanjones99@gmail.com>
2021-01-14 18:39:50 -0500

    some other changes i forgot to commit so idk what i changed

--- transaction-service/src/main/java/com/nimatullo/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/nimatullo/transactionservice/TransactionServiceApplication.java
@@ -3,16 +3,19 @@ package com.nimatullo.transactionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TransactionServiceApplication {
+
 	@Bean
-	public RestTemplate getRestController(){
-		return new RestTemplate();
+	public WebClient.Builder getWebClient(){
+	    return WebClient.builder();
 	}
 
 	public static void main(String[] args) {
--- transaction-service/src/main/java/com/nimatullo/transactionservice/controllers/TransactionController.java
+++ transaction-service/src/main/java/com/nimatullo/transactionservice/controllers/TransactionController.java
@@ -11,13 +11,15 @@ import com.nimatullo.transactionservice.models.TransactionStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.UUID;
@@ -27,8 +29,9 @@ public class TransactionController {
 
     private static final Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
+    @Qualifier("getWebClient")
     @Autowired
-    RestTemplate restTemplate;
+    WebClient.Builder template;
 
     @Autowired
     private DiscoveryClient discoveryClient;
@@ -45,7 +48,7 @@ public class TransactionController {
 
     @RequestMapping(value = "/{partId}")
     public Transaction getTransaction(@PathVariable int partId) {
-        Transaction transaction = new Transaction(UUID.randomUUID(), TransactionStatus.PENDING, getPart(partId), "4843853622714538");
+        Transaction transaction = new Transaction(UUID.randomUUID(), TransactionStatus.PENDING, getPart(partId).block(), "4843853622714538");
         Message<PaymentAuthorizationRequest> transactionCreatedMessage = new Message<>(UUID.randomUUID(), new PaymentAuthorizationRequest(transaction));
         save(transaction, transactionCreatedMessage);
         producer.sendMessage(transactionCreatedMessage);
@@ -58,8 +61,12 @@ public class TransactionController {
 
     }
 
-    private GraphicsCard getPart(int partId) {
-        return restTemplate.getForObject(getServiceURL("zuul-service") + "/parts/" + partId, GraphicsCard.class);
+    private Mono<GraphicsCard> getPart(int partId) {
+        return template.build()
+                .get()
+                .uri(getServiceURL("zuul-service")+"/parts/"+partId)
+                .retrieve()
+                .bodyToMono(GraphicsCard.class);
     }
 
     private String getServiceURL(String serviceName) {
